1d030ad6750b7fc853409f7ab4e4af81b0316e02
vladji <vladjislav@gmail.com>
2024-03-18 16:46:54 +0500

    feat: payment link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,3 @@
-import { PhoneIcon } from '../../icons/PhoneIcon';
 import { EmailIcon } from '../../icons/EmailIcon';
 import { VisaIcon } from '../../icons/VisaIcon';
 import { MastercardIcon } from '../../icons/MastercardIcon';
@@ -21,21 +20,14 @@ export const Footer = () => {
           <div>
             <div className={styles.contacts}>
               <div className={`${styles.row}`}>
-                <PhoneIcon />
-                <a href="tel:+77089128007">+7 708 912 80 07</a>
-              </div>
-              <div className={`${styles.row}`}>
                 <EmailIcon />
-                <a href="mailto:info@neocredit.kz">info@neocredit.kz</a>
+                <a href="mailto:info@i-credit.kz">info@i-credit.kz</a>
               </div>
             </div>
             <div className={styles.cardsContainer}>
               <VisaIcon />
               <MastercardIcon />
             </div>
-          </div>
-          <div className={`desktopDisplay ${styles.license}`}>
-            Номер лицензии ТОО “Legal Money” 05.22.0002.М. от 15.11.2022 года
           </div>
         </div>
         <div className={`${styles.block} ${styles.bottomBlock}`}>
@@ -46,20 +38,16 @@ export const Footer = () => {
           />
           <div className={styles.info}>
             <p className={styles.text}>
-              ТОО «Микрофинансовая организация «LEGAL MONEY»
+              ТОО «I-Credit.kz» г. Алматы, 050057, ул. Толе би, 101
             </p>
             <p className={styles.text}>
-              г. Алматы, 050057ул. Толе би, 101БЦ &quot;Толе би&quot;, 3 этаж,
-              блок D
+              г. Алматы, БЦ "Толе би", 3 этаж, блок D
             </p>
             <p className={styles.text}>
-              БИН: 220440007472ОКЭД: 64929Р/сч: KZ55601A861010717701АО
+              БИН: 070440003902 р/с: KZ55601A861010717701
               &quot;Народный Банк Казахстана&quot; Кбе 15
             </p>
             <p className={styles.text}>БИК: HSBKKZKX</p>
-          </div>
-          <div className={`mobileDisplay ${styles.license}`}>
-            Номер лицензии ТОО “Legal Money” 05.22.0002.М. от 15.11.2022 года
           </div>
         </div>
       </div>
